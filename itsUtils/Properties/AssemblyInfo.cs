87d4ca06e5fb8453f66e3b90e4c4a9908c0f9fb9
carles <unknown@kiln.example.com>
2013-03-22 14:29:35 -0100

    Fixed Nuget package generation.

--- itsUtils/Properties/AssemblyInfo.cs
+++ itsUtils/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("itsUtils")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Utility library for iTextSharp. It only has a PageEvent to add header, footer, page numbers and watermark to the pages.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Carles Company Soler")]
 [assembly: AssemblyProduct("itsUtils")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
